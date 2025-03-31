51d486fd632932e439bdeec0abbcd9e04e687720
AsperSTS <lopezmine426@gmail.com>
2025-03-31 16:59:54 -0600

    Commit 4

--- loadModel5.m
+++ loadModel5.m
@@ -5,14 +5,14 @@ load('modelo5_Circles.mat', 'RNE');
 nuevos_datos = csvread('dataset5_Circles.csv'); % Asegúrate de que este archivo existe
 
 % Separar características de los nuevos datos
-X_nuevos = nuevos_datos(:, 1:16)';
-t = nuevos_datos(:, 17)'; % Columna con etiquetas reales
+X_nuevos = nuevos_datos(:, 1:2)';
+t = nuevos_datos(:, 3)'; % Columna con etiquetas reales
 
 % Normalizar los nuevos datos usando la misma estructura de preprocesamiento (ps)
-% [X_nuevos_norm, ps] = mapminmax(X_nuevos);
+[X_nuevos_norm, ps] = mapminmax(X_nuevos);
 
 % Simular la red neuronal
-y = sim(RNE, X_nuevos);
+y = sim(RNE, X_nuevos_norm);
 
 % Calcular precisión
 m = length(t);
